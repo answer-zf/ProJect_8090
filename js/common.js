3fa26c69f30b58886b5cce851c9da8fbac305de2
answer-zf <feng18255163789@gmail>
2020-04-11 14:38:23 +0800

    commit -m "save"

---
+++ js/common.js
@@ -0,0 +1,16 @@
+$(function () {
+  tab_floatingSwitch('#myTab > li', '#myTabContent > div')
+})
+
+/* tab页悬停切换 */
+function tab_floatingSwitch(id_1, id_2) {
+  $(id_1).hover(function () {
+    $(id_1 + ' > a').removeClass('active')
+    $(this).children().addClass('active')
+
+    $(id_2).removeClass('show active')
+
+    var href = $(this).children().attr('href')
+    $(href).addClass(' show active')
+  })
+}
