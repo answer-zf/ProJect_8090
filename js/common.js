88a1a4150e4b1b22c61c92e129ff665c16b14d7d
answer-zf <feng18255163789@gmail>
2020-04-24 18:00:00 +0800

    home

--- js/common.js
+++ js/common.js
@@ -1,26 +1,26 @@
 $(function () {
-  // 导航吸顶
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 106) {
-      $('#nav-zf').addClass('nav-fix-zf')
+    // 导航吸顶
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 106) {
+            $('#nav-zf').addClass('nav-fix-zf')
+        } else {
+            $('#nav-zf').removeClass('nav-fix-zf')
+        }
+    })
+
+    // 导航高亮
+    var hash = '#' + window.location.pathname.split('.')[0].slice(1)
+
+    if (hash === '#') {
+        $('#nav-zf .nav-link').removeClass('active')
+        $('#index').addClass('active')
     } else {
-      $('#nav-zf').removeClass('nav-fix-zf')
+        $('#nav-zf .nav-link').removeClass('active')
+        $(hash).addClass('active')
     }
-  })
-
-  // 导航高亮
-  var hash = '#' + window.location.pathname.split('.')[0].slice(1)
-
-  if (hash === '#') {
-    $('#nav-zf .nav-link').removeClass('active')
-    $('#index').addClass('active')
-  } else {
-    $('#nav-zf .nav-link').removeClass('active')
-    $(hash).addClass('active')
-  }
 })
 
 // 头部搜索
 function searchHandle() {
-  console.log($('#search-input').val())
+    console.log($('#search-input').val())
 }
