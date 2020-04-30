2f4c94dd533ab6a41b8a10d748a2c1b26694f60f
answer-zf <feng18255163789@gmail.com>
2020-04-30 09:10:15 +0800

    text

--- text.py
+++ text.py
@@ -2,11 +2,10 @@
     
 """
 
-input_string = input("")
-d01 = {}
-for item in input_string:
-    if item not in d01:
-        d01[item] = 1
-    else:
-        d01[item] += 1
-print(d01)
+list01 = ["zfd", "cd", "ws"]
+list02 = [101, 102, 103]
+# dic01 = {item: len(item) for item in list01}
+# print(dic01)
+
+dic02 = {list01[i]: list02[i] for i in range(3)}
+print(dic02)
