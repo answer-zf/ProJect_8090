873dd631b1ef01d9a2d668439e4539fd832ba5a5
answer-zf <feng18255163789@gmail.com>
2020-05-06 17:45:51 +0800

    home

--- text.py
+++ text.py
@@ -4,17 +4,15 @@
 
 
 class Car:
-    # self 所创建的 调用当前方法的对象
+    # self 调用当前方法的对象
     def __init__(self, name, model, price):
         self.name = name
         self.model = model
         self.price = price
 
     def travel(self):
-        print("travel")
+        print(self.name, "travel")
 
 
 w01 = Car("bm", 12, 10000)
 w01.travel()
-
-print(w01)
